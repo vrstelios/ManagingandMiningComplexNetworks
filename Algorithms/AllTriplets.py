62d3523e8bc4776de93b2ced6e522c8b43432de0
vrstelios <verstelios@gmail.com>
2023-12-25 10:45:55 +0200

    I made the algorithm alltriplets

---
+++ Algorithms/AllTriplets.py
@@ -0,0 +1,36 @@
+import time
+import pandas as pd
+import networkx as nx
+
+
+class FoundAllTriplets:
+
+    @staticmethod
+    def found_all_triplets():
+        start_time = time.time()
+
+        df = pd.read_csv('Graphs/tvshow_edges.csv', delimiter=',')
+        G = nx.from_pandas_edgelist(df, 'node_1', 'node_2')
+
+        edges_list = list(G.edges())
+        triangles = set()
+
+        for i in range(0, len(edges_list)-1):
+            for j in range(0, len(edges_list)-1):
+                # check that have same root
+                if edges_list[i][0] == edges_list[j][0]:
+                    # check there are the graph the two edges
+                    if (edges_list[i][1], edges_list[j][1]) in edges_list:
+                        common_node = edges_list[i][0]
+                        node_i = edges_list[i][1]
+                        node_j = edges_list[j][1]
+                        triangles.add(tuple(sorted([common_node, node_i, node_j])))
+
+        print("Triangles are:", triangles)
+        print("Number of triangles:", len(triangles))
+
+        end_time = time.time()
+        print("Execution time:", end_time - start_time)
+
+        # Number of triangles: 87276
+        # Execution time: 161.6605236530304
--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from Algorithms import AllTriplets
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    # call the algorithms
+    # AllTriplets
+    AllTriplets.FoundAllTriplets().found_all_triplets()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
+    main()

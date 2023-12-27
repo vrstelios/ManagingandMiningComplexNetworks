d298619a2efbed7d2f708b2289a6e5663f352acb
vrstelios <verstelios@gmail.com>
2023-12-27 19:42:16 +0200

    fix the allTriplets

--- Algorithms/AllTriplets.py
+++ Algorithms/AllTriplets.py
@@ -12,19 +12,30 @@ class FoundAllTriplets:
         df = pd.read_csv('Graphs/tvshow_edges.csv', delimiter=',')
         G = nx.from_pandas_edgelist(df, 'node_1', 'node_2')
 
-        edges_list = list(G.edges())
+        #edges_list = list(G.edges())
         triangles = set()
 
-        for i in range(0, len(edges_list)-1):
-            for j in range(0, len(edges_list)-1):
-                # check that have same root
-                if edges_list[i][0] == edges_list[j][0]:
-                    # check there are the graph the two edges
-                    if (edges_list[i][1], edges_list[j][1]) in edges_list:
-                        common_node = edges_list[i][0]
-                        node_i = edges_list[i][1]
-                        node_j = edges_list[j][1]
-                        triangles.add(tuple(sorted([common_node, node_i, node_j])))
+        for node in G.nodes():
+            # look at all possible edge pairs
+            for neighbor1 in G.neighbors(node):
+                for neighbor2 in G.neighbors(node):
+                    # check if there is an edge between all possible pairs
+                    if G.has_edge(neighbor1, neighbor2):
+                        # Added the triangles in set
+                        triangles.add(tuple(sorted([node, neighbor1, neighbor2])))
+
+#        for i in range(0, len(edges_list)):
+#            for j in range(0, len(edges_list)):
+#                # check that have same root
+#                if edges_list[i][0] == edges_list[j][0]:
+#                    # check there are the graph the two edges
+#                    if (edges_list[i][1], edges_list[j][1]) in edges_list:
+#                        common_node = edges_list[i][0]
+#                        node_i = edges_list[i][1]
+#                        node_j = edges_list[j][1]
+#                        triangles.add(tuple(sorted([common_node, node_i, node_j])))
+
+
 
         print("Triangles are:", triangles, "For AllTriplets")
         print("Number of triangles:", len(triangles), "For AllTriplets")
@@ -32,5 +43,7 @@ class FoundAllTriplets:
         end_time = time.time()
         print("Execution time:", end_time - start_time)
 
-        # Number of triangles: 87276
-        # Execution time: 161.6605236530304
+        # Number of triangles: 87277
+        # Execution time: 0.6750340461730957
+
+
